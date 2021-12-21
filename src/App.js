a87e4d4144ce25e84c601e727f1e2ac74fbeda97
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2021-12-21 14:23:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FeedbackList from './components/FeedbackList';
+import Header from './components/Header';
+import Card from './components/shared/Card';
+import FeedbackData from './data/FeedbackData';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [feedback, setFeedback] = useState(FeedbackData);
+
+    return (
+        <>
+            <Header bgColor='blue' textColor='red' />
+            <div className="container">
+                <FeedbackList feedback={feedback} />
+            </div>
+        </>
+    )
 }
 
 export default App;
